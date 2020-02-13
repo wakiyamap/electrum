a6b80437498e9e008adaf5a65d57a8e942cafdb4
wakiyamap <wakiyamap@gmail.com>
2020-02-14 02:46:40 +0900

    Fix build

--- contrib/build-wine/prepare-wine.sh
+++ contrib/build-wine/prepare-wine.sh
@@ -107,14 +107,14 @@ cp "$CACHEDIR/libusb/libusb/.libs/libusb-1.0.dll" $WINEPREFIX/drive_c/tmp/  || f
 
 
 # copy libsecp dll (already built)
-cp "$PROJECT_ROOT/electrum_mona/libsecp256k1-0.dll" $WINEPREFIX/drive_c/tmp/ || fail "Could not copy libsecp to its destination"
+cp "$PROJECT_ROOT/electrum-mona/libsecp256k1-0.dll" $WINEPREFIX/drive_c/tmp/ || fail "Could not copy libsecp to its destination"
 
 
 info "Building PyInstaller."
 # we build our own PyInstaller boot loader as the default one has high
 # anti-virus false positives
 (
-    cd "$WINEPREFIX/drive_c/electrum_mona"
+    cd "$WINEPREFIX/drive_c/electrum-mona"
     ELECTRUM_COMMIT_HASH=$(git rev-parse HEAD)
     cd "$CACHEDIR"
     rm -rf pyinstaller
